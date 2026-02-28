e0b8a4988df25952b117f3e3e04d93fd8410153c
Diana Shadiyeva <diana.shadiyeva@gcore.lu>
2026-02-28 12:52:23 +0500

    Initial Django project created

---
+++ tasks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    completed = models.BooleanField(default=False)
