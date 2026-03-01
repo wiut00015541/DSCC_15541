30469dc6d1b03b0d4bd72ddab8e251e584a132dd
Diana Shadiyeva <diana.shadiyeva@gcore.lu>
2026-03-01 12:20:35 +0500

    Basic HTML created fot the task view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
