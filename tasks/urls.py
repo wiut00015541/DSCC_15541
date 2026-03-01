83462c678942e860bbc1d49711ae8d7e8980bca0
Diana Shadiyeva <diana.shadiyeva@gcore.lu>
2026-03-01 12:31:39 +0500

    Add user registration

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
+    path('register/', views.register, name='register'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
 from .models import Task
+
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'task_list.html', {'tasks': tasks})
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
