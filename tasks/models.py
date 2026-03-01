2ffd9f79283e408493c28af549c27cb9c13c63f2
Diana Shadiyeva <diana.shadiyeva@gcore.lu>
2026-03-01 12:07:27 +0500

    Category and Tasks added with relationships

--- tasks/models.py
+++ tasks/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -11,3 +15,6 @@ class Task(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     categories = models.ManyToManyField(Category)
     completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
